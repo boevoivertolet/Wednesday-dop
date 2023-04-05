15931d9ada71b95c005dfe9cd88d96a6695c9715
boevoivertolet <bjha78rus@yandex.ru>
2023-04-05 17:38:13 +0300

    Task_3 useEffect

--- extra_classes_wednesday_2.3/src/task_3/slowComponent/SlowComponent.tsx
+++ extra_classes_wednesday_2.3/src/task_3/slowComponent/SlowComponent.tsx
@@ -1,13 +1,21 @@
-import {memo} from "react";
+import {memo, useEffect} from "react";
 
 export const SlowComponent = memo(() => {
     console.log('SlowComponent re-render...');
 
     let now = performance.now();
 
-    while (performance.now() - now < 1000) {
-        // Artificial delay -- do nothing for 100ms
-    }
+   // useMemo(()=> {
+   //     while (performance.now() - now < 1000) {
+   //         // Artificial delay -- do nothing for 100ms
+   //     }
+   // },[])
+
+    useEffect(()=> {
+        while (performance.now() - now < 1000) {
+            // Artificial delay -- do nothing for 100ms
+        }
+    },[])
 
     return <p>I am a very slow component tree.</p>;
 });
